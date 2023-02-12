6589de88259a59d8aaebabb6c0df1e70cf2cc71a
Ruben Madsen <ruben@rubenmadsen.com>
2023-02-12 13:29:05 +0000

    task 3 done

--- task2/main.c
+++ task2/main.c
@@ -6,15 +6,27 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdbool.h>
-#include <wait.h>
+#include <sys/wait.h>
 #include <string.h>
+#include <sys/resource.h>
+#include <time.h>
 
-void findStrInStr(char *text, char *str);
+unsigned int findStrInStr(char *head, char *end, char *str);
 int main(int argc, char ** argv){
+    //uint64_t *shared_results = mmap(NULL,facs*sizeof(uint64_t),PROT_READ | PROT_WRITE,MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    struct rlimit limit;
+    getrlimit (RLIMIT_STACK, &limit);
+    printf ("\nStack Limit = %ld and %ld max\n", limit.rlim_cur, limit.rlim_max);
     bool isParent = false;
+    //
+    clock_t tos = clock();
+    unsigned int *total;
+    total = malloc(sizeof(unsigned int));
+    *total = 0;
     FILE *fileptr;
     size_t fsize;
-    fileptr = fopen("../bible.txt","r");
+    fileptr = fopen(argv[1],"r");
     if(!fileptr) {
         printf("Grande open problemas\n");
         exit(1);
@@ -24,24 +36,18 @@ int main(int argc, char ** argv){
     rewind(fileptr);
     printf("opened file (%zu bytes) ",fsize);
     size_t part = fsize/4;
-    char parts[4][part];
-    printf("parts will be %zu bytes each\n",part);
-    for(int i=0; i<4; i++){
-        char *partptr = parts[i];
-        size_t bytes_read = fread(partptr,1,part,fileptr);
-        if(bytes_read == -1){
-            printf("Grande fread problemas\n");
-            exit(1);
-        }
-        printf("Read %zu bytes\n",bytes_read);
-    }
-    char *ptr = parts[0];
-    findStrInStr(ptr,"beginning");
+    char bytes[fsize];
+    fread(bytes,1,fsize,fileptr);
+    printf("Searching '%s' for '%s'\n",argv[1],argv[2]);
+//    findStrInStr(bytes,bytes+part,argv[2]);
+//    findStrInStr(bytes,bytes+part,argv[2]);
+
     // start forking disaster
     for (int i = 0; i < 4; i++) {
         pid_t pid = fork();
         if (pid == 0) {
             // childs
+            *total += findStrInStr(bytes+(part*i),bytes+part+(part*i),argv[2]);
 
             exit(0);
         }
@@ -51,31 +57,41 @@ int main(int argc, char ** argv){
         pid_t pid = wait(NULL);
         printf("Process %d is terminated.\n", pid);
     }
+    printf("Found: %d results\n",*total);
+    clock_t toe = clock();
+    double elapsed_time = (double)(toe - tos) / CLOCKS_PER_SEC;
+    printf("Total time: %f ms\n", elapsed_time);
     return 0;
 }
-void findStrInStr(char *text, char *str){
-
-    char *head = text;
-    char *end = text+strlen(text);
+unsigned int findStrInStr(char *head, char *end, char *str){
+    clock_t start = clock();
+    unsigned int found = 0;
+    //char *head = text;
+    //char *end = text+strlen(text);
     int row = 0;
     int col = 0;
+    pid_t main = getpid();
     size_t len = strlen(str);
-    char *tail = text+len;
-    int stop = 0;
-    head++;
-    stop = 1;
-        while(tail != end){
-        if(strncmp(head,str,len) == 0){
-            printf("Text found row %d col %d\n",row,col);
-            return;
+    char *tail = head+len;
+
+        while(tail != end) {
+            if (strncmp(head, str, len) == 0) {
+                //printf("Text found row %6d col %3d\n", row, col);
+                //return;
+                found++;
+            }
+            col++;
+            if (head[0] == '\n') {
+                row++;
+                col = 0;
+            }
+            head++;
+            tail++;
         }
-        col++;
-        if(head[0] == '\n'){
-            row++;
-            col = 0;
-        }
-        head++;
-        tail++;
-    }
-    printf("Text not found\n");
+    //printf("found %d\n",found);
+    clock_t ending = clock();
+    double elapsed_time = (double)(ending - start) / CLOCKS_PER_SEC;
+    printf("Elapsed time: %fs\n", elapsed_time);
+    return found;
+    //printf("Text not found\n");
 }
---
+++ task3/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+uint64_t factorial(int f){
+    uint64_t result = 1;
+    uint64_t sum = 0;
+    for (int i=1; i<=f;i++) {
+        result *= i;
+        sum += result;
+        printf("%lu ", result);
+    }
+
+    printf("\n");
+    return sum;
+}
+int main(int argc, char* args[]) {
+
+    int facs = atoi(args[1]);
+    pid_t id = fork();
+    uint64_t sums[facs];
+    //sums[0] = 1;
+    if(id == 0){
+        uint64_t sum = 0;
+        for(int i=1; i<=facs; i++){
+            sums[i] = factorial(i);
+            //printf("[SUM]=%lu |",sums[i-1]);
+        }
+        printf("\n");
+        printf("The sum of the series is\n\n");
+        for(int i=1; i<=facs; i++){
+            printf("%lu ",sums[i]);
+        }
+        printf("\n");
+
+        exit(0);
+    }
+    wait(NULL);
+    return 0;
+}
